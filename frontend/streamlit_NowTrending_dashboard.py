265547b03db5ac07fc429d55d2873f5af1652cbe
Maria Zreik <maria.zreik@outlook.com>
2025-02-06 09:37:42 +0000

    Added Streamlit secrets.

--- frontend/streamlit_NowTrending_dashboard.py
+++ frontend/streamlit_NowTrending_dashboard.py
@@ -63,7 +63,7 @@ st.markdown(
     <style>
     /* Adjusting the main content area */
     .main .block-container {
-        max-width: 70%; /* Reduce the width to create a shift */
+        max-width: 65%; /* Reduce the width to create a shift */
         margin-left: -10%;
         margin-right: 15%; /* Shift towards the sidebar */
     }
--- scripts/fetch_twitter_data.py
+++ scripts/fetch_twitter_data.py
@@ -1,16 +1,16 @@
 import requests
-import sys
+# import sys
 import json
-import os
+# import os
 import streamlit as st
 
-# Extend sys path to access the database module
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from config import TWITTER_API_KEY, TWITTER_API_HOST 
+# # Extend sys path to access the database module
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# from config import TWITTER_API_KEY, TWITTER_API_HOST 
 
 # Twitter Hashflags API
-TWITTER_API_KEY=st.secrets["TWITTER_API_KEY"]["TWITTER_API_KEY"]
-TWITTER_API_HOST=st.secrets["TWITTER_API_HOST"]["TWITTER_API_HOST"]
+TWITTER_API_KEY=st.secrets["TWITTER_API_KEY"]
+TWITTER_API_HOST=st.secrets["TWITTER_API_HOST"]
 
 
 # API URLs for Twitter Data
